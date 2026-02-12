7880aa582c8a167a70b7c8c4fa4bd9349d37d9f0
murillo <2612391@escolas.anchieta.br>
2026-02-12 11:11:44 -0300

    aula 1

---
+++ entrada.py
@@ -0,0 +1,11 @@
+entrada = input()
+
+print(entrada)
+
+nome = input('Qual o seu nome? ')
+senha = input('Qual a sua senha? ')
+dominio = input('Qual o seu dominio? ')
+print('ola', nome,',seja bem vindo ao meu mundo') 
+print('Sua senha é:', senha)
+print('o seu dominio é: ', dominio)
+print('seu nome é:' nome  , 'e seu dominio:', dominio)
