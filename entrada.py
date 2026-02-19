4fd034b1cc6ca546c0e0fc52624cd7c4aa1cb9a8
murillo <2612391@escolas.anchieta.br>
2026-02-19 09:46:16 -0300

    segundo commit

--- entrada.py
+++ entrada.py
@@ -5,7 +5,20 @@ print(entrada)
 nome = input('Qual o seu nome? ')
 senha = input('Qual a sua senha? ')
 dominio = input('Qual o seu dominio? ')
-print('ola', nome,',seja bem vindo ao meu mundo') 
-print('Sua senha é:', senha)
+print('ola ',nome, ', seja bem vindo ao meu mundo') 
+print('Sua senha é: ',  senha)
 print('o seu dominio é: ', dominio)
-print('seu nome é:' nome  , 'e seu dominio:', dominio)
+
+
+email = nome + '@' + dominio
+print('seu e-mail é :', email)
+
+palavra = 'maça'
+#colocar a string como toda maiuscúla
+print('colocando o texto em maiuscúla: ', palavra.upper())
+
+PALAVRA = 'MAÇA'
+#colocar a string como toda minuscula
+print('colocando o texto em minuscula: ', PALAVRA.lower())  
+
+
