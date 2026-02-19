902da5eeaab2140650f6942a113694dadc1185b1
murillo <2612391@escolas.anchieta.br>
2026-02-19 10:56:30 -0300

    segundo commit

--- entrada.py
+++ entrada.py
@@ -21,4 +21,22 @@ PALAVRA = 'MAÇA'
 #colocar a string como toda minuscula
 print('colocando o texto em minuscula: ', PALAVRA.lower())  
 
+#contar caracter da string 
+palavra_contar = 'banana'
+print('contar a letra b', palavra_contar.count('b') )
+print('contar a letra a', palavra_contar.count('a') )
+print('contar a letra n', palavra_contar.count('n') )
+
+x = palavra_contar.count('x')
+print('a palavra x foi contada ', x)
+
+print(email)
+a_c = email.count('a')
+e_c = email.count('e')
+i_c = email.count('i')
+o_c = email.count('o')
+u_c = email.count('u')
+nova_senha = 'a' + str(a_c) + 'e' + str(e_c) + 'i' + str(i_c) + 'o' + str(o_c) + 'u' + str(u_c)
+print(nova_senha)
+
 
